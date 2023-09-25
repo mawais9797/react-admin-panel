bd72c0f788d5b4659a60500503be1a06ce255677
Awais <awais@gmail.com>
2023-09-25 19:42:40 +0500

    First commit

---
+++ src/scenes/global/Topbar.js
@@ -0,0 +1,8 @@
+import React, { useContext } from "react";
+import { Box, IconButton, useTheme } from "@mui/material";
+
+const Topbar = () => {
+  return <div>Topbar</div>;
+};
+
+export default Topbar;
